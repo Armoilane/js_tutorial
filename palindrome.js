7a574f12c10d9bdbd19b7dc4c17a631339b03b21
Armoilane <armo@iki.fi>
2020-04-04 17:32:38 +0200

    Object trickery with the palindromes

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,37 @@
-// Reverses a string
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds reverse a all strings
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
+String.prototype.blank = function() {
+  return !!this.match(/\s|^\s+$/);
+}
+
+Array.prototype.last = function() {
+  return this.slice(-1);
+}
+
+// Returns true for a palindrome, false otherwise
+// function palindrome(string) {
+// let processedContent = string.toLowerCase();
+// return processedContent === reverse(processedContent);
+// }
+
+// Defines a Phrase object
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true for a palindrome
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
 }
