427989a768ff6683661c6110625837d6d5c05b77
Armoilane <armo@iki.fi>
2020-04-08 11:58:56 +0200

    Result to a dynamic HTML element

--- bundle.js
+++ bundle.js
@@ -4,15 +4,23 @@ let Phrase = require("armo-palindrome");
 function palindromeTester() {
   let string = prompt("Please enter a string for palindrome testing:");
   let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
   
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+    palindromeResult.innerHTML = 
+      `<strong>"${phrase.content}"</strong> is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+    palindromeResult.innerHTML = 
+      `<strong>"${phrase.content}"</strong> is not a palindrome.`;
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+/*   let form = document.querySelector("#palindromeTester");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+  });
+ */
   let button = document.querySelector("#palindromeTester");
   button.addEventListener("click", function() {
     palindromeTester();
